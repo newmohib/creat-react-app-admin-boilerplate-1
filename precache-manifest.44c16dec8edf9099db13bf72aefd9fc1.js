ff973f90c9a285fa4d5e9ba475d72acce4239963
unknown <newmohib@gmail.com>
2020-05-10 21:06:03 +0600

    Updates

---
+++ precache-manifest.44c16dec8edf9099db13bf72aefd9fc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfcb8596c682a5b9014a042cf55fea8f",
+    "url": "/creat-react-app-admin-boilerplate-1/index.html"
+  },
+  {
+    "revision": "00542975c23035aaf988",
+    "url": "/creat-react-app-admin-boilerplate-1/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a59e5182a9c5810422cc",
+    "url": "/creat-react-app-admin-boilerplate-1/static/css/main.b4e9390d.chunk.css"
+  },
+  {
+    "revision": "00542975c23035aaf988",
+    "url": "/creat-react-app-admin-boilerplate-1/static/js/2.4123c8ac.chunk.js"
+  },
+  {
+    "revision": "2244c70a4d0dbad5ca6f65005d865f09",
+    "url": "/creat-react-app-admin-boilerplate-1/static/js/2.4123c8ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a59e5182a9c5810422cc",
+    "url": "/creat-react-app-admin-boilerplate-1/static/js/main.9503c0ea.chunk.js"
+  },
+  {
+    "revision": "c1445884c7f594c934d0",
+    "url": "/creat-react-app-admin-boilerplate-1/static/js/runtime-main.26a20f15.js"
+  }
+]);
